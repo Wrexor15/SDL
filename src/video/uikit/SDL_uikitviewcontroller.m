18d012999286cb79f1f8468087f47befe2155d64
Wrexor <wrexor@wrexor.net>
2025-11-06 18:50:05 +0100

    fixes

--- src/video/uikit/SDL_uikitviewcontroller.m
+++ src/video/uikit/SDL_uikitviewcontroller.m
@@ -611,7 +611,7 @@ SDL_bool UIKit_HasScreenKeyboardSupport(_THIS)
     return SDL_TRUE;
 }
 
-void UIKit_ShowScreenKeyboard(_THIS, SDL_Window *window, SDL_bool)
+void UIKit_ShowScreenKeyboard(_THIS, SDL_Window *window, SDL_bool password)
 {
     @autoreleasepool {
         SDL_uikitviewcontroller *vc = GetWindowViewController(window);
